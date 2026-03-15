68d3337f7dd60236b9a5c505dd65c3cd7db9cfac
Edilan Batista Gomes De Lima <134861238+EdilanGomes@users.noreply.github.com>
2026-03-15 17:48:40 -0300

    ffff

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,10 +24,9 @@ const trustedLogos = [
 "/softwares/googleads.png",
 "/softwares/rd.png",
 "/softwares/edrone.png",
-"/softwares/rdstation.png",
 "/softwares/melhorenvio.png",
 "/softwares/bling.png",
-"/softwares/hostgator.png",
+"/softwares/hostgatir.png",
 "/softwares/tiktokads.png"
 ];
 
