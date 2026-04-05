12a61ceb98f01256f09b10d4ea5ab1019a1215e0
Edilan Batista Gomes De Lima <134861238+EdilanGomes@users.noreply.github.com>
2026-04-05 17:57:24 -0300

    cayria

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -69,6 +69,10 @@ export default function CTA() {
         </div>
       </div>
 
+      <!-- Elfsight Google Reviews | Untitled Google Reviews -->
+<script src="https://elfsightcdn.com/platform.js" async></script>
+<div class="elfsight-app-34396a43-1e6b-4b8f-a10a-f6d6560f9a69" data-elfsight-app-lazy></div>
+
     </section>
   );
 }
